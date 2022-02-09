e4eaa27a743f47b1616e0f9860d5922b418d6bd3
Md. Raihanul Islam Rafi <r.rafi.aiub@gmail.com>
2022-02-09 11:34:37 +0600

    php

---
+++ MID_LAB_TASK/MID_LAB_TASK_04_PHP/4.php
@@ -0,0 +1,19 @@
+<?php
+  $n1 = (float)readline('Enter 1st Number : ');
+  $n2 = (float)readline('Enter 2nd Number : ');
+  $n3 = (float)readline('Enter 3rd Number : ');
+  function large($x, $y, $z) {
+      $max = $x;
+
+      if ($x >= $y && $x >= $z)
+        $max = $x;
+      elseif ($y >= $x && $y >= $z)
+        $max = $y;
+      else 
+       $max = $z;
+  
+  echo "Largest number among $x, $y and $z is: $max\n";
+  }
+
+large($n1, $n2, $n3);
+?>
