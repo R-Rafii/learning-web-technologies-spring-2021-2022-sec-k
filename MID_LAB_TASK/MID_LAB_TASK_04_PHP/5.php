b3030687560071be05c0959381c6182f3212a4d3
Md. Raihanul Islam Rafi <r.rafi.aiub@gmail.com>
2022-02-09 11:57:05 +0600

    php

---
+++ MID_LAB_TASK/MID_LAB_TASK_04_PHP/5.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo "Odd number from 10 to 100  :";
+
+    for($a=10;$a<100;$a++)
+        {
+            if($a%2!=0)
+        {
+            echo $a ."<br>";
+        }
+        else 
+        {
+         continue;
+        }
+}
+
+?>
