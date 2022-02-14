63074e2c61e188ce676b371b4518aa3794332241
Md. Raihanul Islam Rafi <r.rafi.aiub@gmail.com>
2022-02-15 02:16:54 +0600

    ...

---
+++ MID_LAB_TASK/MID_LAB_TASK_05_PHP_FORM/3A.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Date of Birth</title>
+</head>
+<body>
+	<fieldset>
+		<legend>Date of Birth</legend>
+			<form method="POST" action="handler.php">
+				<input type="date" name="dob" value="" /><br>
+				<hr style="width:9%;text-align:left;margin-left:2"></hr>
+				<input type="submit" name="dobSubmit" value="Submit">
+			</form>
+	</fieldset>
+	<br>
+</body>
+</html>
--- MID_LAB_TASK/MID_LAB_TASK_05_PHP_FORM/handler.php
+++ MID_LAB_TASK/MID_LAB_TASK_05_PHP_FORM/handler.php
@@ -1,13 +1,29 @@
 <?php
-	if(isset($_REQUEST['nameSubmit'])){
-
-		
-		
+	if(isset($_REQUEST['nameSubmit']))
+	{
 		if($_REQUEST['name'] == null){
 			echo "name is empty....";
 		}
 		else{
 			echo "Name : ".$_REQUEST['name'];
+		}
+	}
+	if(isset($_REQUEST['emailSubmit']))
+	{
+		if($_REQUEST['email'] == null){
+			echo "Email is empty....";
+		}
+		else{
+			echo "Eamil : ".$_REQUEST['email'];
+		}
+	}
+	if(isset($_REQUEST['dobSubmit']))
+	{
+		if($_REQUEST['dob'] == null){
+			echo "Date of birth is empty....";
+		}
+		else{
+			echo "Date of birth : ".$_REQUEST['dob'];
 		}
 	}
 	
