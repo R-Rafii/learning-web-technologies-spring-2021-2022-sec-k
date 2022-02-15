633ee50fb2b90a26327ba3121bb2389576a360e6
Md. Raihanul Islam Rafi <r.rafi.aiub@gmail.com>
2022-02-15 21:58:43 +0600

    php

---
+++ MID_LAB_TASK/MID_LAB_TASK_05_PHP_FORM/4/handler.php
@@ -0,0 +1,49 @@
+<?php
+	if(isset($_REQUEST['nameSubmit']))
+	{
+		if($_REQUEST['name'] == null){
+			echo "name is empty....";
+		}
+		else{
+			echo "Name : ".$_REQUEST['name'];
+		}
+	}
+	if(isset($_REQUEST['emailSubmit']))
+	{
+		if($_REQUEST['email'] == null){
+			echo "Email is empty....";
+		}
+		else{
+			echo "Eamil : ".$_REQUEST['email'];
+		}
+	}
+	if(isset($_REQUEST['dobSubmit']))
+	{
+		if($_REQUEST['dob'] == null){
+			echo "Date of birth is empty....";
+		}
+		else{
+			echo "Date of birth : ".$_REQUEST['dob'];
+		}
+	}
+	if(isset($_REQUEST['genderSubmit']))
+	{
+		if(empty($_REQUEST['gender']))
+		{
+			echo "Gender is empty....";
+		}
+		else{
+			echo "Gender : ".$_REQUEST['gender'];
+		}
+	}
+	if(isset($_REQUEST['dSubmit']))
+	{
+		if(empty($_REQUEST['d']))
+		{
+			echo "Degree : ".$_REQUEST['d'];
+		}
+		else{
+			echo "Degree : ".$_REQUEST['d'];
+		}	
+	}
+?>
