9c20a72ecf760f2489af819e1d7e07fd0273808b
Md. Raihanul Islam Rafi <r.rafi.aiub@gmail.com>
2022-02-16 11:47:44 +0600

    php

---
+++ MID_LAB_TASK/MID_LAB_TASK_06_PHP_SESSION/1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Blood Group</title>
+</head>
+<body>
+	<table border="1" width="100%">
+		<tr>
+			<td rowspan="4" >Public User</td>
+			<td>Public Home</td>
+		</tr>
+		<tr>
+			<td>Registration</td>
+		</tr>
+		<tr>
+			<td>Login</td>
+		</tr>
+		<tr>
+			<td>Forgot Password</td>
+		</tr>
+	</table>
+</body>
+</html>
